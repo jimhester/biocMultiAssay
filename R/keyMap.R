ba97db307e004b1ce9ce8fe7038e160a73e47308
Jim Hester <james.f.hester@gmail.com>
2015-10-15 10:33:28 -0400

    Example implementation of mapIDToRanges

--- R/keyMap.R
+++ R/keyMap.R
@@ -1,3 +1,70 @@
+setGeneric("mapIDToRanges", signature="db",
+    function(db, ...) standardGeneric("mapIDToRanges")
+)
+
+#setMethod("mapIDToRanges", "Multiassay",
+    #function(db, ...)
+    #{
+        ## Extract x, keys, keytype etc. from the Multiassay class
+        ## Dispatch to method below based on 'x':
+        #mapIDToRanges(db, keytype, columns, level, ...)
+    #}
+#)
+
+setMethod("mapIDToRanges", "TxDb",
+          function(db,
+                   vals,
+                   keytype = c("cds", "exon", "tx", "gene"),
+                   by = keytype,
+                   column = NULL,
+                   ...)
+{
+    keytype <- match.arg(keytype)
+    by <- match.arg(by, c("cds", "exon", "tx", "gene"))
+
+    if (keytype == by) {
+        fun <- switch(keytype,
+                      cds = cds,
+                      exon = exons,
+                      tx = transcripts,
+                      gene = genes)
+        res <- fun(db, vals, column = unique(c(names(vals), column)), ...)
+        matches <- BiocGenerics::match(mcols(res)[[names(vals)]], vals[[1]])
+        ranges <- rep(res, vapply(matches, length, integer(1)))
+        mcols(ranges)[[names(vals)]] <- NULL
+        split(ranges, vals[[1]][BiocGenerics::unlist(matches)])
+    } else {
+        fun <- switch(by,
+               cds = cdsBy,
+               exon = exonsBy,
+               tx = transcriptsBy)
+        res <- fun(db, keytype, ..., use.names = TRUE)
+        res[vals[[1]]]
+    }
+})
+
+#exons()/transcripts()/cds() -> findOverlaps()
+
+#setMethod("mapIDToRanges", "OrganismDb",
+    #function(db, keys, keytype = NULL, columns = NULL, level =
+#c("exon", "cds", "gene", etc.), ...)
+    #{
+        #stopifnot(keytype)
+        ## Check that keytype is in columns(x)
+        ## Check that keytype is a valid map between OrgDb and TxDb objects
+            #eg, ENSEMBLTRANS -> ENTREZID is not
+        ## Decide what columns are needed to create GRanges based on level
+        ## Dispatch to low level mapper
+    #}
+#)
+
+#setMethod("mapIDToRanges", "biomaRt",
+    #function(x, keys, keytype=someDefault, columns=someDefault, level =
+#c("exon", "cds", "gene", etc.), ...)
+    #{
+     #...
+    #}
+#)
 #' Map a set of keys and return a corresponding range
 #' @param db Database to use
 #' @param keytype keytype passed to \code{select()}, if \code{NULL} uses the first keytype.
@@ -11,42 +78,57 @@
 #' mapKey(Homo.sapiens)
 #' # also works with orgDb objects, although the seqinfo is not populated
 #' mapKey(org.Hs.eg.db)
-mapKey <- function(db, keytype = NULL, keys = NULL, type = c("cds", "exon", "transcript"), columns = NULL) {
+#mapKey <- function((db), key, ...)
 
-    if (is.null(columns)) {
-        type <- match.arg(type)
+#TxDb <- function(db, keys = "ENL", keytype = c("cds", "exon", "tx", "gene"), by = keytype) { #}c("cds", "exon", "tx", "gene")) {
+    #if (keytype == by) {
+        #switch(keytype,
+               #tx = transcript,
+               #exon = exon,
+               #)
+    #} else {
+    #switch(keytype,
+           #tx = transcriptsBy(db, keys, by = by, ...)
+           #exon = exonsBy(db, keys, by = by, ...)
+         #)
+    #}
 
-        prefix <- switch(type,
-            cds = "CDS",
-            exon = "EXON",
-            transcript = "TX"
-            )
+#mapKey <- function(db, vals, valstype = c("cds", "exon", "transcript", "gene"), type = c("cds", "exon", "transcript", "gene"), columns = NULL) {
 
-        columns <- paste0(prefix, c("CHROM", "START", "END", "STRAND"))
-    }
+    #if (is.null(columns)) {
+        #type <- match.arg(type)
 
-    if (is.null(keytype)) {
-        keytype <- keytypes(db)[1L]
-    }
+        #prefix <- switch(type,
+            #cds = "CDS",
+            #exon = "EXON",
+            #transcript = "TX"
+            #)
 
-    if (is.null(keys)) {
-        keys <- head(keys(db, keytype), n = 3)
-    }
-    seqInfo <- tryCatch(seqinfo(db), error = function(e) NULL)
+        #columns <- paste0(prefix, c("CHROM", "START", "END", "STRAND"))
+    #}
 
-    res <- suppressMessages(
-        AnnotationDbi::select(db,
-            keytype = keytype,
-            columns = columns,
-            keys = keys))
+    #if (is.null(keytype)) {
+        #keytype <- keytypes(db)[1L]
+    #}
 
-    split(
-        makeGRangesFromDataFrame(res[, -1, drop = FALSE],
-            seqinfo = seqInfo,
-            keep.extra.columns = TRUE,
-            seqnames.field = columns[1],
-            start.field = columns[2],
-            end.field = columns[3],
-            strand.field = columns[4]),
-        res[[keytype]])
-}
+    #if (is.null(keys)) {
+        #keys <- head(keys(db, keytype), n = 3)
+    #}
+    #seqInfo <- tryCatch(seqinfo(db), error = function(e) NULL)
+
+    #res <- suppressMessages(
+        #AnnotationDbi::select(db,
+            #keytype = keytype,
+            #columns = columns,
+            #keys = keys))
+
+    #split(
+        #makeGRangesFromDataFrame(res[, -1, drop = FALSE],
+            #seqinfo = seqInfo,
+            #keep.extra.columns = TRUE,
+            #seqnames.field = columns[1],
+            #start.field = columns[2],
+            #end.field = columns[3],
+            #strand.field = columns[4]),
+        #res[[keytype]])
+#}
